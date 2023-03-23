56a9663d8df6a1b7321cba87cd884eb2aaca3ed9
SeokHyun Youn <cheongyeon2018@gmail.com>
2023-03-23 19:35:38 +0900

    Response 수정

--- router/event_post.py
+++ router/event_post.py
@@ -21,7 +21,7 @@ class EventHeader(BaseModel):
     CameraId: int
 
 class EventBody(BaseModel):
-    Label: int
+    Label: str
     Left: int
     Top: int
     Right: int
@@ -58,7 +58,7 @@ def create_event(header: EventHeader, response: Response):
         for bbox, cls in zip(result.boxes.xyxy, result.boxes.cls):
             left, top, right, bottom = bbox.tolist()
             event_bodies.append({
-                'Label': int(cls.item()),
+                'Label': str(cls.item()),
                 'Left': left,
                 'Top': top,
                 'Right': right,
